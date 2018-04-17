c63b2e60347b154cf4f854905883fa98b13e6669
Valentin Knabel <dev@vknabel.com>
2018-04-17 12:59:58 +0200

    compatabilityTestAdditionsForUrl may actually have been optional

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,7 +5,7 @@ let mayHaveAddedProjects = danger.github.modifiedFiles.contains("README.md")
 
 if (mayHaveAddedProjects) {
     let diffUrl = "https://github.com/\(danger.github.pullRequest.fullName)/pull/\(danger.github.pullRequest.number)"
-    let results = compatabilityTestAdditionsForUrl(diffUrl)
+    let results = compatabilityTestAdditionsForUrl(diffUrl) ?? []
     for (repo, error) in results {
         #if os(Linux)
         let osName = "Linux"
