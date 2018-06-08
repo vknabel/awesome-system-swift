94a917bb46955c1853fcfe3034badba50a9a8e0a
Valentin Knabel <dev@vknabel.com>
2018-06-08 17:51:44 +0200

    Catch errors by compat

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -6,18 +6,22 @@ let mayHaveAddedProjects = danger.git.modifiedFiles.contains("README.md")
 
 if (mayHaveAddedProjects) {
     let diffUrl = "https://github.com/\(danger.github.pullRequest.base.repo.fullName)/pull/\(danger.github.pullRequest.number).diff"
-    let results = compatabilityTestAdditionsForUrl(diffUrl) ?? []
-    for summary in results {
-        #if os(Linux)
-        let osName = "Linux"
-        #else
-        let osName = "macOS"
-        #endif
-        switch summary {
-        case let .compatible(repo):
-            message("\(repo.name) seems to be compatible with \(osName)")
-        case let .incompatible(repo, reason):
-            fail("\(repo.name) seems to be incompatible with \(osName): \(reason)")
+    do {
+        let results = try compatabilityTestAdditionsForUrl(diffUrl)
+        for summary in results {
+            #if os(Linux)
+            let osName = "Linux"
+            #else
+            let osName = "macOS"
+            #endif
+            switch summary {
+            case let .compatible(repo):
+                message("\(repo.name) seems to be compatible with \(osName)")
+            case let .incompatible(repo, reason):
+                fail("\(repo.name) seems to be incompatible with \(osName): \(reason)")
+            }
         }
+    } catch {
+        warn("Danger failed: \(error)")
     }
 }
